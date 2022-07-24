6d83e38b4fb61dd7a70fcf5d433f518de494041e
daniel.arisa@yahoo.com <daaniel.arisa@yahoo.com>
2022-07-24 02:50:08 -0500

    commit tree 3ecdb6b38fccf534f375aab6180dbbc18a402ba0

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const cased = tutorials.map((cb) => {
+      const newCase = cb
+          .split(' ')
+          .map((word) => {
+              return word.charAt(0).toUpperCase() + word.slice(1)
+          })
+          .join(' ')
+
+      return newCase
+  })
+  return cased
 }
